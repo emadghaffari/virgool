44b2a487f5bd620d0564cbb82b7f28fc05c8cfa5
Emad <emadghaffariii@gmail.com>
2021-01-09 21:44:55 +0000

    [init] add basic cmd configs

--- notification/cmd/main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import service "github.com/emadghaffari/virgool/notification/cmd/service"
-
-func main() {
-	service.Run()
-}
--- notification/cmd/service/service.go
+++ notification/cmd/service/service.go
@@ -3,6 +3,7 @@ package service
 import (
 	"flag"
 	"fmt"
+	"io"
 	"net"
 	"net/http"
 	"os"
@@ -12,12 +13,24 @@ import (
 	endpoint1 "github.com/go-kit/kit/endpoint"
 	log "github.com/go-kit/kit/log"
 	prometheus "github.com/go-kit/kit/metrics/prometheus"
+	"github.com/grpc-ecosystem/grpc-opentracing/go/otgrpc"
 	group "github.com/oklog/oklog/pkg/group"
 	opentracinggo "github.com/opentracing/opentracing-go"
 	prometheus1 "github.com/prometheus/client_golang/prometheus"
 	promhttp "github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/sirupsen/logrus"
+	"github.com/uber/jaeger-client-go"
+	jaegercfg "github.com/uber/jaeger-client-go/config"
+	jaegerlog "github.com/uber/jaeger-client-go/log"
+	"github.com/uber/jaeger-lib/metrics"
 	grpc1 "google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 
+	"github.com/emadghaffari/virgool/notification/conf"
+	"github.com/emadghaffari/virgool/notification/database/kafka"
+	"github.com/emadghaffari/virgool/notification/database/redis"
+	"github.com/emadghaffari/virgool/notification/env"
+	"github.com/emadghaffari/virgool/notification/model"
 	endpoint "github.com/emadghaffari/virgool/notification/pkg/endpoint"
 	grpc "github.com/emadghaffari/virgool/notification/pkg/grpc"
 	pb "github.com/emadghaffari/virgool/notification/pkg/grpc/pb"
@@ -30,18 +43,38 @@ var logger log.Logger
 // Define our flags. Your service probably won't need to bind listeners for
 // all* supported transports, but we do it here for demonstration purposes.
 var fs = flag.NewFlagSet("notification", flag.ExitOnError)
-var debugAddr = fs.String("debug.addr", ":8080", "Debug and metrics listen address")
-var httpAddr = fs.String("http-addr", ":8081", "HTTP listen address")
-var grpcAddr = fs.String("grpc-addr", ":8082", "gRPC listen address")
 var thriftAddr = fs.String("thrift-addr", ":8083", "Thrift listen address")
 var thriftProtocol = fs.String("thrift-protocol", "binary", "binary, compact, json, simplejson")
 var thriftBuffer = fs.Int("thrift-buffer", 0, "0 for unbuffered")
 var thriftFramed = fs.Bool("thrift-framed", false, "true to enable framing")
-var zipkinURL = fs.String("zipkin-url", "", "Enable Zipkin tracing via a collector URL e.g. http://localhost:9411/api/v1/spans")
-var lightstepToken = fs.String("lightstep-token", "", "Enable LightStep tracing via a LightStep access token")
-var appdashAddr = fs.String("appdash-addr", "", "Enable Appdash tracing via an Appdash server host:port")
 
+// Run func
 func Run() {
+
+	// Read configs
+	if err := initConfigs(); err != nil {
+		logger.Log("exit")
+		return
+	}
+
+	// conf logger
+	conf.ConfigureLogging(&conf.GlobalConfigs.Log)
+
+	// connect to local initRedis
+	if err := initRedis(); err != nil {
+		logger.Log("exit")
+		return
+	}
+
+	// connect to kafka
+	if err := initKafka(); err != nil {
+		logger.Log("exit")
+		return
+	}
+
+	// validate
+	model.Validator.New()
+
 	fs.Parse(os.Args[1:])
 
 	// Create a single logger, which we'll use and give to other components.
@@ -51,7 +84,14 @@ func Run() {
 
 	//  Determine which tracer to use. We'll pass the tracer to all the
 	// components that use it, as a dependency
-	// TODO init jaeger tracer
+	//  Determine which tracer to use. We'll pass the tracer to all the
+	// components that use it, as a dependency
+	closer, err := initJaeger()
+	if err != nil {
+		logger.Log("exit")
+		return
+	}
+	defer closer.Close()
 
 	svc := service.New(getServiceMiddleware(logger))
 	eps := endpoint.New(svc, getEndpointMiddleware(logger))
@@ -66,13 +106,23 @@ func initGRPCHandler(endpoints endpoint.Endpoints, g *group.Group) {
 	// Add your GRPC options here
 
 	grpcServer := grpc.NewGRPCServer(endpoints, options)
-	grpcListener, err := net.Listen("tcp", *grpcAddr)
+	grpcListener, err := net.Listen("tcp", conf.GlobalConfigs.GRPC.Port)
 	if err != nil {
 		logger.Log("transport", "gRPC", "during", "Listen", "err", err)
 	}
 	g.Add(func() error {
-		logger.Log("transport", "gRPC", "addr", *grpcAddr)
-		baseServer := grpc1.NewServer()
+		logger.Log("transport", "gRPC", "addr", conf.GlobalConfigs.GRPC.Port)
+
+		// UnaryInterceptor and OpenTracingServerInterceptor for tracer
+		baseServer := grpc1.NewServer(
+			grpc1.UnaryInterceptor(
+				otgrpc.OpenTracingServerInterceptor(tracer, otgrpc.LogPayloads()),
+			),
+		)
+
+		// reflection for evans
+		reflection.Register(baseServer)
+
 		pb.RegisterNotificationServer(baseServer, grpcServer)
 		return baseServer.Serve(grpcListener)
 	}, func(error) {
@@ -82,7 +132,7 @@ func initGRPCHandler(endpoints endpoint.Endpoints, g *group.Group) {
 }
 func getServiceMiddleware(logger log.Logger) (mw []service.Middleware) {
 	mw = []service.Middleware{}
-	mw = addDefaultServiceMiddleware(logger, mw)
+	mw = append(mw, service.LoggingMiddleware(logger))
 	// Append your middleware here
 
 	return
@@ -102,12 +152,12 @@ func getEndpointMiddleware(logger log.Logger) (mw map[string][]endpoint1.Middlew
 }
 func initMetricsEndpoint(g *group.Group) {
 	http.DefaultServeMux.Handle("/metrics", promhttp.Handler())
-	debugListener, err := net.Listen("tcp", *debugAddr)
+	debugListener, err := net.Listen("tcp", conf.GlobalConfigs.DEBUG.Port)
 	if err != nil {
 		logger.Log("transport", "debug/HTTP", "during", "Listen", "err", err)
 	}
 	g.Add(func() error {
-		logger.Log("transport", "debug/HTTP", "addr", *debugAddr)
+		logger.Log("transport", "debug/HTTP", "addr", conf.GlobalConfigs.DEBUG.Port)
 		return http.Serve(debugListener, http.DefaultServeMux)
 	}, func(error) {
 		debugListener.Close()
@@ -127,4 +177,56 @@ func initCancelInterrupt(g *group.Group) {
 	}, func(error) {
 		close(cancelInterrupt)
 	})
+}
+
+func initConfigs() error {
+	// Current working directory
+	dir, err := os.Getwd()
+	if err != nil {
+		logrus.Warn(err.Error())
+	}
+	// read from file
+	return env.LoadGlobalConfiguration(dir + "/auth/config.yaml")
+}
+
+func initJaeger() (io.Closer, error) {
+	// Sample configuration for testing. Use constant sampling to sample every trace
+	// and enable LogSpan to log every span via configured Logger.
+	cfg := jaegercfg.Configuration{
+		ServiceName: conf.GlobalConfigs.Service.Name,
+		Sampler: &jaegercfg.SamplerConfig{
+			Type:  jaeger.SamplerTypeConst,
+			Param: 1,
+		},
+		Reporter: &jaegercfg.ReporterConfig{
+			LogSpans:           conf.GlobalConfigs.Jaeger.LogSpans,
+			LocalAgentHostPort: conf.GlobalConfigs.Jaeger.HostPort,
+		},
+	}
+
+	jLogger := jaegerlog.StdLogger
+	jMetricsFactory := metrics.NullFactory
+
+	// Initialize tracer with a logger and a metrics factory
+	var closer io.Closer
+	var err error
+	tracer, closer, err = cfg.NewTracer(
+		jaegercfg.Logger(jLogger),
+		jaegercfg.Metrics(jMetricsFactory),
+		jaegercfg.ZipkinSharedRPCSpan(true),
+	)
+	if err != nil {
+		logger.Log("during", "Listen", "jaeger", "err", err)
+		return nil, err
+	}
+
+	opentracinggo.SetGlobalTracer(tracer)
+	return closer, nil
+}
+
+func initRedis() error {
+	return redis.Database.Connect(&conf.GlobalConfigs)
+}
+func initKafka() error {
+	return kafka.Database.Connect(&conf.GlobalConfigs)
 }
