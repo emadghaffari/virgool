57c7ee03ae6b199dd4a8fe8929d513aaf953d4eb
Emad <emadghaffariii@gmail.com>
2021-01-09 21:44:22 +0000

    [init] add basic db/kafka

---
+++ notification/database/kafka/kafka_dao.go
@@ -0,0 +1,23 @@
+package kafka
+
+import (
+	"fmt"
+
+	"github.com/emadghaffari/virgool/notification/conf"
+)
+
+func (k *kf) Validate(config *conf.GlobalConfiguration) error {
+	if len(config.Kafka.Brokers) == 0 {
+		return fmt.Errorf("kafka need Brokers, please set the Brokers")
+	}
+	if config.Kafka.Version == "" {
+		return fmt.Errorf("kafka need Version, please set the Version")
+	}
+	if config.Kafka.Topics.Notif == "" {
+		return fmt.Errorf("kafka need Topics, please set the Topics")
+	}
+	if config.Kafka.Assignor == "" {
+		return fmt.Errorf("kafka need Assignor, please set the Assignor")
+	}
+	return nil
+}
