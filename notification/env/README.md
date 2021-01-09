89e9216a8676dadb58f24d69ce779337c16087af
Emad <emadghaffariii@gmail.com>
2021-01-09 21:43:50 +0000

    [init] add basic env

---
+++ notification/env/README.md
@@ -0,0 +1,7 @@
+# production example
+
+### key:
+confs
+
+### value:
+{"GRPC":{"Host":"localhost","Port":":8082","Endpoint":":8083"},"HTTP":{"Host":"localhost","Port":":8080","Endpoint":":8081"},"DEBUG":{"Host":"localhost","Port":":8084","Endpoint":":8085"},"MYSQL":{"Username":"root","Password":"password","Host":"db","Schema":"virgool","Driver":"mysql","Automigrate":true,"Logger":true,"Namespace":""},"Redis":{"Username":"","Password":"","DB":0,"Host":"redis:6379","Logger":false},"Vault":{"Address":"http://vault:8200","Token":"s.0mW3pqMEbW0W9yYXTpSDtQPD"},"MultiInstanceMode":false,"Log":{"disable_colors":false,"quote_empty_fields":false},"Service":{"Name":"auth"},"Jaeger":{"HostPort":"jaeger:6831","LogSpans":true}}
---
+++ notification/env/env.go
@@ -0,0 +1,82 @@
+package env
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+
+	"github.com/emadghaffari/virgool/notification/conf"
+	"github.com/emadghaffari/virgool/notification/database/vault"
+)
+
+// LoadGlobalConfiguration returns configs
+func LoadGlobalConfiguration(filename string) error {
+
+	if err := localEnvironment(filename); err != nil {
+		return err
+	}
+
+	if viper.GetString("environment") == "production" {
+		return vaultEnvironment()
+	}
+
+	return nil
+}
+
+func localEnvironment(filename string) error {
+
+	// name of config file (without extension)
+	// REQUIRED if the config file does not have the extension in the name
+	// path to look for the config file in
+	viper.SetConfigFile(filename)
+
+	if err := viper.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			// Config file not found; ignore error if desired
+			logrus.WithFields(logrus.Fields{
+				"error": fmt.Sprintf("Config file not found; ignore error if desired: %s", err),
+			}).Fatal(fmt.Sprintf("Config file not found; ignore error if desired: %s", err))
+		} else {
+			// Config file was found but another error was produced
+			logrus.WithFields(logrus.Fields{
+				"error": fmt.Sprintf("Config file was found but another error was produced: %s", err),
+			}).Fatal(fmt.Sprintf("Config file was found but another error was produced: %s", err))
+		}
+		return err
+	}
+
+	if err := viper.Unmarshal(&conf.GlobalConfigs); err != nil {
+		// Config file can not unmarshal to struct
+		logrus.WithFields(logrus.Fields{
+			"error": fmt.Sprintf("Config file can not unmarshal to struct: %s", err),
+		}).Fatal(fmt.Sprintf("Config file can not unmarshal to struct: %s", err))
+
+		return err
+	}
+
+	return nil
+}
+
+func vaultEnvironment() error {
+	// Vault connection
+	if err := vault.Database.New(conf.GlobalConfigs); err != nil {
+		fmt.Fprintf(os.Stderr, ": %v\n", err)
+		return err
+	}
+
+	r, err := vault.Database.Read(viper.GetString("vault.configuration"))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, ": %v\n", err)
+		return err
+	}
+
+	if err := json.Unmarshal([]byte(r.Data["confs"].(string)), &conf.GlobalConfigs); err != nil {
+		fmt.Fprintf(os.Stderr, ": %v\n", err)
+		return err
+	}
+
+	return nil
+}
