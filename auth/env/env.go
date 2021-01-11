2eba6ec45e949f86234029f7c7954e8569111164
Emad <emadghaffariii@gmail.com>
2021-01-11 16:11:25 +0000

    [fix] check local env file exists or not

--- auth/env/env.go
+++ auth/env/env.go
@@ -15,8 +15,10 @@ import (
 // LoadGlobalConfiguration returns configs
 func LoadGlobalConfiguration(filename string) error {
 
-	if err := localEnvironment(filename); err != nil {
-		return err
+	if _, err := os.Stat(filename); os.IsExist(err) {
+		if err := localEnvironment(filename); err != nil {
+			return err
+		}
 	}
 
 	if viper.GetString("environment") == "production" {
