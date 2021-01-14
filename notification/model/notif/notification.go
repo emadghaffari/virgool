dead6979a0fb8e7c437220477c3bb9951adb66e8
Emad <emadghaffariii@gmail.com>
2021-01-14 13:08:46 +0000

    [option] add sms with templates and bodies

--- notification/model/notif/notification.go
+++ notification/model/notif/notification.go
@@ -7,16 +7,22 @@ import (
 
 // Notifier interface
 type Notifier interface {
-	Send(ctx context.Context, notif Notification, param []SMSParams, to, template string) error
+	SendWithTemplate(ctx context.Context, notif Notification, params []SMSParams, template string) error
+	SendWithBody(ctx context.Context, notif Notification, options ...Option) error
 }
 
 // Notification struct
 type Notification struct {
-	Data    interface{} `json:"data"`
-	Message string      `json:"message"`
-	KEY     string      `json:"key"`
-	Type    string      `json:"type"`
+	Data         interface{} `json:"data"`
+	Message      string      `json:"message"`
+	KEY          string      `json:"key"`
+	Type         string      `json:"type"`
+	sendDateTime string      `default:""`
+	line         string      `default:""`
 }
+
+// Option func for SMS
+type Option func(*Notification)
 
 // GetNotifier func
 func GetNotifier(t string) (Notifier, error) {
@@ -25,5 +31,22 @@ func GetNotifier(t string) (Notifier, error) {
 		return new(SMS), nil
 	default:
 		return nil, fmt.Errorf("Not Found")
+	}
+}
+
+// SendDateTime Option func
+// you can specifically send a notif in specific time
+func SendDateTime(time string) Option {
+	return func(n *Notification) {
+		n.sendDateTime = time
+	}
+}
+
+// Line Option func
+// you can specifically send a notif from a line
+// examples: PHONE_NUMBER, EMAIL_ADDRESS,....
+func Line(line string) Option {
+	return func(n *Notification) {
+		n.line = line
 	}
 }
--- notification/model/notif/sms.go
+++ notification/model/notif/sms.go
@@ -26,115 +26,237 @@ type SMSParams struct {
 	ParameterValue interface{}
 }
 
-// Send meth
-func (s *SMS) Send(ctx context.Context, notif Notification, params []SMSParams, to, template string) error {
-	fmt.Println(to)
-	fmt.Println(template)
-	t, err := s.token()
-	if err != nil {
-		return err
+// SendWithTemplate meth
+// Need Phone in Data Params
+func (s *SMS) SendWithTemplate(ctx context.Context, notif Notification, params []SMSParams, template string) error {
+	// Validate Phone number exists in notif
+	mobile, ok := notif.Data.(map[string]interface{})["phone"].(string)
+	if !ok {
+		logrus.WithFields(logrus.Fields{
+			"error": fmt.Sprintf("Error in Get Phone Number From Notif Data: %v", notif),
+		}).Warn(fmt.Sprintf("Error in Get Phone Number From Notif Data: %v", notif))
+		return fmt.Errorf("Phone not exists")
 	}
 
-	client := http.Client{
-		Timeout: time.Second * 5,
-	}
-
+	// Marshal Data We Need
 	body, err := json.Marshal(map[string]interface{}{
 		"ParameterArray": params,
-		"Mobile":         to,
+		"Mobile":         mobile,
 		"TemplateId":     template,
 	})
 	if err != nil {
 		logrus.WithFields(logrus.Fields{
-			"error": fmt.Sprintf("Error in Marshal Send SMS Data: %s", err),
-		}).Warn(fmt.Sprintf("Error in Marshal Send SMS Data: %s", err))
+			"error": fmt.Sprintf("Error in Marshal Send SMS Data: %s", err.Error()),
+		}).Warn(fmt.Sprintf("Error in Marshal Send SMS Data: %s", err.Error()))
 		return err
 	}
 
-	request, err := http.NewRequest("POST", conf.GlobalConfigs.Notif.SMS.Send.TemplateURL, bytes.NewBuffer(body))
-	request.Header.Set("x-sms-ir-secure-token", t)
-	request.Header.Set("Content-Type", conf.GlobalConfigs.Notif.SMS.Send.Verify.ContentType)
-
-	resp, err := client.Do(request)
-	if err != nil {
-		logrus.WithFields(logrus.Fields{
-			"error": fmt.Sprintf("Error in Send SMS: %s", err),
-		}).Warn(fmt.Sprintf("Error in Send SMS: %s", err))
-		return err
-	}
-	defer resp.Body.Close()
-
-	bt, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		logrus.WithFields(logrus.Fields{
-			"error": fmt.Sprintf("Error in Read Response Body After Send SMS: %s", err),
-			"msg":   string(bt),
-		}).Warn(fmt.Sprintf("Error in Read Response Body After Send SMS: %s", err))
-		return err
+	headers := []struct {
+		Header string
+		Vaule  string
+	}{
+		{
+			"Content-Type",
+			conf.GlobalConfigs.Notif.SMS.Send.Verify.ContentType,
+		},
 	}
 
-	if err := json.Unmarshal(bt, &s); err != nil {
-		logrus.WithFields(logrus.Fields{
-			"error": fmt.Sprintf("Error in Unmarshal Response Data For Sended SMS: %s", err),
-		}).Warn(fmt.Sprintf("Error in Unmarshal Response Data For Sended SMS: %s", err))
-		return err
-	}
-
-	if s.IsSuccessful == false {
-		logrus.WithFields(logrus.Fields{
-			"error": fmt.Sprintf("Error in Send SMS: %s", err),
-			"body":  string(bt),
-		}).Warn(fmt.Sprintf("Error in Send SMS: %s", err))
+	if err := s.send("POST", conf.GlobalConfigs.Notif.SMS.Send.TemplateURL, headers, body); err != nil {
 		return err
 	}
 
 	return nil
 }
 
-func (s *SMS) token() (string, error) {
-	req, err := json.Marshal(map[string]string{
+// SendWithBody meth
+// Need Phone,Body in Data Params
+func (s *SMS) SendWithBody(ctx context.Context, notif Notification, options ...Option) error {
+	// Validate Phone number exists in notif
+	mobile, ok := notif.Data.(map[string]interface{})["phone"].(string)
+	if !ok {
+		logrus.WithFields(logrus.Fields{
+			"error": fmt.Sprintf("Error in Get Phone Number From Notif Data: %v", notif),
+		}).Warn(fmt.Sprintf("Error in Get Phone Number From Notif Data: %v", notif))
+		return fmt.Errorf("Phone not exists")
+	}
+
+	// FIXME change phone into body
+	// Validate Phone number exists in notif
+	txt, ok := notif.Data.(map[string]interface{})["phone"].(string)
+	if !ok {
+		logrus.WithFields(logrus.Fields{
+			"error": fmt.Sprintf("Error in Get Phone Number From Notif Data: %v", notif),
+		}).Warn(fmt.Sprintf("Error in Get Phone Number From Notif Data: %v", notif))
+		return fmt.Errorf("Phone not exists")
+	}
+
+	for _, option := range options {
+		option(&notif)
+	}
+
+	// Marshal Data We Need
+	body, err := json.Marshal(map[string]interface{}{
+		"Messages":                 []string{txt},
+		"MobileNumbers":            []string{mobile},
+		"LineNumber":               notif.line,
+		"SendDateTime":             notif.sendDateTime,
+		"CanContinueInCaseOfError": "false",
+	})
+	if err != nil {
+		logrus.WithFields(logrus.Fields{
+			"error": fmt.Sprintf("Error in Marshal Send SMS Data: %s", err.Error()),
+		}).Warn(fmt.Sprintf("Error in Marshal Send SMS Data: %s", err.Error()))
+		return err
+	}
+
+	headers := []struct {
+		Header string
+		Vaule  string
+	}{
+		{
+			"Content-Type",
+			conf.GlobalConfigs.Notif.SMS.Send.Verify.ContentType,
+		},
+	}
+	if err := s.send("POST", conf.GlobalConfigs.Notif.SMS.Send.BodyURL, headers, body); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Get Token We Need for every SMS We Send from sms.ir
+func (s *SMS) token() error {
+	// try to Marshal Api key, Secret key
+	body, err := json.Marshal(map[string]string{
 		"UserApiKey": conf.GlobalConfigs.Notif.SMS.UserAPIKey,
 		"SecretKey":  conf.GlobalConfigs.Notif.SMS.SecretKey,
 	})
 	if err != nil {
 		logrus.WithFields(logrus.Fields{
-			"error": fmt.Sprintf("Error in Marshal Data sms.go: %s", err),
-		}).Warn(fmt.Sprintf("Error in Marshal Data sms.go: %s", err))
-		return "", err
+			"error": fmt.Sprintf("Error in Marshal Data sms.go: %s", err.Error()),
+		}).Warn(fmt.Sprintf("Error in Marshal Data sms.go: %s", err.Error()))
+		return err
 	}
 
-	resp, err := http.Post(conf.GlobalConfigs.Notif.SMS.Token.URL, conf.GlobalConfigs.Notif.SMS.Token.ContentType, bytes.NewBuffer(req))
+	headers := []struct {
+		Header string
+		Vaule  string
+	}{
+		{
+			"Content-Type",
+			conf.GlobalConfigs.Notif.SMS.Send.Verify.ContentType,
+		},
+	}
+
+	// New Client
+	client := http.Client{
+		Timeout: time.Second * 5,
+	}
+
+	// Start POST Request
+	request, err := http.NewRequest("POST", conf.GlobalConfigs.Notif.SMS.Token.URL, bytes.NewBuffer(body))
+	for _, v := range headers {
+		request.Header.Set(v.Header, v.Vaule)
+	}
+
+	// Client try To Send
+	resp, err := client.Do(request)
 	if err != nil {
 		logrus.WithFields(logrus.Fields{
-			"error": fmt.Sprintf("Error in Post Data For Get SMS Token: %s", err),
-		}).Warn(fmt.Sprintf("Error in Post Data For Get SMS Token: %s", err))
-		return "", err
+			"error": fmt.Sprintf("Error in Send SMS: %s", err.Error()),
+		}).Warn(fmt.Sprintf("Error in Send SMS: %s", err.Error()))
+		return err
 	}
-
 	defer resp.Body.Close()
 
-	body, err := ioutil.ReadAll(resp.Body)
+	// Try To Read Response
+	bt, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		logrus.WithFields(logrus.Fields{
-			"error": fmt.Sprintf("Error ioutil Read Data: %s", err),
-		}).Warn(fmt.Sprintf("Error ioutil Read Data: %s", err))
-		return "", err
+			"error": fmt.Sprintf("Error in Read Response Body After Send SMS: %s", err.Error()),
+			"msg":   string(bt),
+		}).Warn(fmt.Sprintf("Error in Read Response Body After Send SMS: %s", err.Error()))
+		return err
 	}
 
-	if err := json.Unmarshal(body, &s); err != nil {
+	// Try To Unmarshal Data
+	if err := json.Unmarshal(bt, &s); err != nil {
 		logrus.WithFields(logrus.Fields{
-			"error": fmt.Sprintf("Error in Unmarshal Response Post Token SMS: %s", err),
-		}).Warn(fmt.Sprintf("Error in Unmarshal Response Post Token SMS: %s", err))
-		return "", err
+			"error": fmt.Sprintf("Error in Unmarshal Response Data For Sended SMS: %s", err.Error()),
+		}).Warn(fmt.Sprintf("Error in Unmarshal Response Data For Sended SMS: %s", err.Error()))
+		return err
 	}
 
+	// Check if IsSuccessful or Not!
 	if s.IsSuccessful == false {
 		logrus.WithFields(logrus.Fields{
-			"error": fmt.Sprintf("Error in Get Send SMS Token: %s", err),
-			"body":  string(body),
-		}).Warn(fmt.Sprintf("Error in Get Send SMS Token: %s", err))
-		return "", err
+			"error": fmt.Sprintf("Error in Send SMS: %s", string(bt)),
+			"body":  string(bt),
+		}).Warn(fmt.Sprintf("Error in Send SMS: %s", string(bt)))
+		return err
 	}
 
-	return s.TokenKey, nil
+	return nil
+}
+
+func (s *SMS) send(method, url string, headers []struct {
+	Header string
+	Vaule  string
+}, body []byte) error {
+
+	// Get Token From Service if TokenKey is empty
+	if err := s.token(); err != nil {
+		return err
+	}
+
+	// New Client
+	client := http.Client{
+		Timeout: time.Second * 5,
+	}
+
+	// Start POST Request
+	request, err := http.NewRequest(method, url, bytes.NewBuffer(body))
+	for _, v := range headers {
+		request.Header.Set(v.Header, v.Vaule)
+	}
+	request.Header.Set("x-sms-ir-secure-token", s.TokenKey)
+
+	// Client try To Send
+	resp, err := client.Do(request)
+	if err != nil {
+		logrus.WithFields(logrus.Fields{
+			"error": fmt.Sprintf("Error in Send SMS: %s", err.Error()),
+		}).Warn(fmt.Sprintf("Error in Send SMS: %s", err.Error()))
+		return err
+	}
+	defer resp.Body.Close()
+
+	// Try To Read Response
+	bt, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		logrus.WithFields(logrus.Fields{
+			"error": fmt.Sprintf("Error in Read Response Body After Send SMS: %s", err.Error()),
+			"msg":   string(bt),
+		}).Warn(fmt.Sprintf("Error in Read Response Body After Send SMS: %s", err.Error()))
+		return err
+	}
+
+	// Try To Unmarshal Data
+	if err := json.Unmarshal(bt, &s); err != nil {
+		logrus.WithFields(logrus.Fields{
+			"error": fmt.Sprintf("Error in Unmarshal Response Data For Sended SMS: %s", err.Error()),
+		}).Warn(fmt.Sprintf("Error in Unmarshal Response Data For Sended SMS: %s", err.Error()))
+		return err
+	}
+
+	// Check if IsSuccessful or Not!
+	if s.IsSuccessful == false {
+		logrus.WithFields(logrus.Fields{
+			"error": fmt.Sprintf("Error in Send SMS: %s", string(bt)),
+		}).Warn(fmt.Sprintf("Error in Send SMS"))
+		return err
+	}
+
+	return nil
 }
