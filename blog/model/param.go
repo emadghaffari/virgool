3acfb67fec3e624defd8adfcd2020a47de3d296a
Emad <emadghaffariii@gmail.com>
2021-01-16 14:42:21 +0000

    [init] add params to blog service

---
+++ blog/model/param.go
@@ -0,0 +1,17 @@
+package model
+
+import "time"
+
+// Query struct
+type Query struct {
+	Name  string `validate:"required" json:"name" gorm:"type:varchar(80);"`
+	Value string `validate:"required" json:"value" gorm:"type:varchar(120);"`
+}
+
+// Param struct
+type Param struct {
+	Query
+	id        uint64    `json:"-" gorm:"primaryKey"`
+	CreatedAt time.Time `json:"-"`
+	UpdatedAt time.Time `json:"-"`
+}
