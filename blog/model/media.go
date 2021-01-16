c5e43cbb63b3e2cb3786ddca4b7c3733bfc525f0
Emad <emadghaffariii@gmail.com>
2021-01-16 14:42:11 +0000

    [init] add media type to blog service

---
+++ blog/model/media.go
@@ -0,0 +1,15 @@
+package model
+
+import "time"
+
+// Media struct
+type Media struct {
+	id          uint64    `json:"-" gorm:"primaryKey"`
+	URL         string    `validate:"required" json:"url" gorm:"uniqueIndex"`
+	Type        string    `validate:"required" json:"type"`
+	Title       *string   `validate:"required" json:"title"`
+	Description *string   `validate:"required" json:"description"`
+	Post        []*Post   `json:"posts" gorm:"many2many:posts_medias;association_foreignkey:ID;foreignkey:ID"`
+	CreatedAt   time.Time `json:"-"`
+	UpdatedAt   time.Time `json:"-"`
+}
