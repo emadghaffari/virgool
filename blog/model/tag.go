2a9691f8cee63744db5f217bb8807fc7a5978fea
Emad <emadghaffariii@gmail.com>
2021-01-22 12:19:40 +0000

    [refactor] refactor many to many relation

--- blog/model/tag.go
+++ blog/model/tag.go
@@ -5,7 +5,7 @@ import "time"
 // Tag struct
 type Tag struct {
 	ID        uint64    `json:"-" gorm:"primaryKey"`
-	Posts     []*Post   `json:"posts" gorm:"many2many:posts_params;"`
+	Posts     []*Post   `json:"posts" gorm:"many2many:posts_tags;"`
 	Name      string    `validate:"required" json:"name" gorm:"uniqueIndex;type:varchar(40);"`
 	CreatedAt time.Time `json:"-"`
 	UpdatedAt time.Time `json:"-"`
