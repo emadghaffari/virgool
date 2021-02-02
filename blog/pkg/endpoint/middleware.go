7ddc49bc2f90240b9ad431a10b4e216c339ef333
Emad <emadghaffariii@gmail.com>
2021-02-03 02:00:25 +0700

    [cleanup] cleanup and refactor pkd endpoint

--- blog/pkg/endpoint/middleware.go
+++ blog/pkg/endpoint/middleware.go
@@ -8,6 +8,7 @@ import (
 	endpoint "github.com/go-kit/kit/endpoint"
 	log "github.com/go-kit/kit/log"
 	metrics "github.com/go-kit/kit/metrics"
+	"github.com/sirupsen/logrus"
 )
 
 // InstrumentingMiddleware returns an endpoint middleware that records
@@ -31,7 +32,10 @@ func LoggingMiddleware(logger log.Logger) endpoint.Middleware {
 	return func(next endpoint.Endpoint) endpoint.Endpoint {
 		return func(ctx context.Context, request interface{}) (response interface{}, err error) {
 			defer func(begin time.Time) {
-				logger.Log("transport_error", err, "took", time.Since(begin))
+				err := logger.Log("transport_error", err, "took", time.Since(begin))
+				if err != nil {
+					logrus.Warn(err.Error())
+				}
 			}(time.Now())
 			return next(ctx, request)
 		}
