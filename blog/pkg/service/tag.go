aaf4174336a03fdd99a474a4a00c6420af956310
Emad <emadghaffariii@gmail.com>
2021-02-12 15:43:43 +0000

    [create] compelete create method

--- blog/pkg/service/tag.go
+++ blog/pkg/service/tag.go
@@ -3,14 +3,40 @@ package service
 import (
 	"context"
 	"fmt"
+	"strconv"
+
+	"github.com/opentracing/opentracing-go"
 
 	el "github.com/emadghaffari/virgool/blog/database/elastic"
+	"github.com/emadghaffari/virgool/blog/database/mysql"
 	model "github.com/emadghaffari/virgool/blog/model"
 )
 
 func (b *basicBlogService) CreateTag(ctx context.Context, name string, token string) (message string, status string, err error) {
-	// TODO implement the business logic of CreateTag
-	return message, status, err
+	tracer := opentracing.GlobalTracer()
+	span := tracer.StartSpan("create-tag")
+	defer span.Finish()
+
+	// begins a transaction
+	tx := mysql.Database.GetDatabase().Begin()
+
+	tag := model.Tag{
+		Name: name,
+	}
+
+	if err:= tx.Save(&tag).Error; err != nil{
+		return fmt.Sprintf("error in store a new tag: %s",err.Error()),"ERROR",err
+	}
+
+	tx.Commit()
+
+	if _, err := el.Database.Store(ctx, "tag", "_doc", strconv.Itoa(int(tag.ID)), tag); err != nil {
+		tx.Rollback()
+		return "error in store tag into search engine", "ERROR", fmt.Errorf(fmt.Sprintf("error in store tag into search engine %s", err.Error()))
+	}
+
+
+	return "new tag stored successfully", "SUCCESS", nil
 }
 func (b *basicBlogService) GetTag(ctx context.Context, filter []*model.Query, token string) (tags []*model.Tag, message string, status string, err error) {
 	// TODO implement the business logic of GetTag
@@ -24,7 +50,7 @@ func (b *basicBlogService) DeleteTag(ctx context.Context, name string, token str
 
 	del, err := el.Database.Delete(ctx, "tag", "_doc", name)
 	if err != nil {
-		return fmt.Sprintf("error in delete a post: %s - deleted ID: %s", err, del.Id), "ERROR", err
+		return fmt.Sprintf("error in delete a tag: %s - deleted ID: %s", err, del.Id), "ERROR", err
 	}
 
 	return message, status, err
