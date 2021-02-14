3617d938bbd5f05c72ef225383bbfcecbb5a42ac
Emad <emadghaffariii@gmail.com>
2021-02-14 18:34:29 +0000

    [comment] write comment for codes

--- blog/pkg/service/tag.go
+++ blog/pkg/service/tag.go
@@ -16,6 +16,8 @@ import (
 // CreateTag
 // create and store new tag into database and elasticsearch
 func (b *basicBlogService) CreateTag(ctx context.Context, name string, token string) (message string, status string, err error) {
+
+	// start create-tag tracer
 	tracer := opentracing.GlobalTracer()
 	span := tracer.StartSpan("create-tag")
 	defer span.Finish()
@@ -27,12 +29,15 @@ func (b *basicBlogService) CreateTag(ctx context.Context, name string, token str
 		Name: name,
 	}
 
+	// try to store new tag to database
 	if err := tx.Save(&tag).Error; err != nil {
 		return fmt.Sprintf("error in store a new tag: %s", err.Error()), "ERROR", err
 	}
 
+	// try to commit
 	tx.Commit()
 
+	// try to store tags into elastic
 	if _, err := el.Database.Store(ctx, "tag", "_doc", strconv.Itoa(int(tag.ID)), tag); err != nil {
 		tx.Rollback()
 		return "error in store tag into search engine", "ERROR", fmt.Errorf(fmt.Sprintf("error in store tag into search engine %s", err.Error()))
@@ -44,6 +49,13 @@ func (b *basicBlogService) CreateTag(ctx context.Context, name string, token str
 // GetTag
 // for get a tag with filters from elastic
 func (b *basicBlogService) GetTag(ctx context.Context, filter []*model.Query, token string) (tags []*model.Tag, message string, status string, err error) {
+
+	// start get-tag tracer
+	tracer := opentracing.GlobalTracer()
+	span := tracer.StartSpan("get-tag")
+	defer span.Finish()
+
+	// build query with filters
 	query, err := el.Database.BuildQuery(
 		el.FilterQuery(filter),
 	)
@@ -51,21 +63,29 @@ func (b *basicBlogService) GetTag(ctx context.Context, filter []*model.Query, to
 	if err != nil {
 		return nil, "Failed To Create a Search Query", "500", fmt.Errorf("Failed To Create a Search Query: %s", err)
 	}
+
+	// search tags with query
 	result, err := el.Database.Search(ctx, "tag", query)
 	if err != nil {
 		return nil, "Failed To Search", "500", fmt.Errorf("Failed To Search: %s", err)
 	}
 
+	// loop over hits and unmarshal the tags
 	for _, hit := range result.Hits.Hits {
 		var tag model.Tag
 		if err := json.Unmarshal(hit.Source, &tag); err != nil {
 			return tags, "Failed To Unmarshal Data", "500", fmt.Errorf("Failed To Unmarshal Data: %v", hit)
 		}
+
+		// append tags
 		tags = append(tags, &tag)
 	}
 
 	return tags, "tags searched successfully", "SUCCESS", nil
 }
+
+// UpdateTag
+// replace old tag name and to new name
 func (b *basicBlogService) UpdateTag(ctx context.Context, oldName string, newName string, token string) (message string, status string, err error) {
 	// TODO implement the business logic of UpdateTag
 	return message, status, err
