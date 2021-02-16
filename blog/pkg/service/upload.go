3128c3e8cac942bbc78467d87af383215091354e
Emad <emadghaffariii@gmail.com>
2021-02-16 19:40:47 +0000

    [upload] check and send data to upload method

--- blog/pkg/service/upload.go
+++ blog/pkg/service/upload.go
@@ -3,9 +3,19 @@ package service
 import (
 	"bytes"
 	"context"
+
+	"github.com/emadghaffari/virgool/blog/utils/upload"
 )
 
 func (b *basicBlogService) Upload(ctx context.Context, title string, description string, fileType string, file bytes.Buffer, token string) (message string, status string, err error) {
-	// TODO implement the business logic of Upload
-	return message, status, err
+
+	disk := upload.NewDiskFileStore("public/upload")
+	disk.File.Title = &title
+	disk.File.Description = &description
+
+	if _, err := disk.Store(fileType, file); err != nil {
+		return "error in upload image", "ERROR", err
+	}
+
+	return "successfully file uploaded", "SUCCESS", nil
 }
